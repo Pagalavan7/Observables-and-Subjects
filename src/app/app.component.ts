f06edfb418400be6c0395d240468a5a378be88de
Pagalavan7 <pagalavanvetri@gmail.com>
2024-09-22 09:19:52 +0530

    observable learn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'observables';
+
+  x1() {
+    return new Observable((x) => {
+      x.next(1);
+    });
+  }
+
+  ngOnInit() {
+    const res = this.x1().subscribe();
+    console.log(res);
+  }
 }
